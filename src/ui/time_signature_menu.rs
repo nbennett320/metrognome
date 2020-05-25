16dfc2c3f444b59b148bf330b5c12ebac6abe5ee
Noah Bennett <nbennett.320@gmail.com>
2020-05-25 18:43:54 -0400

    default to 4/4

--- src/ui/time_signature_menu.rs
+++ src/ui/time_signature_menu.rs
@@ -4,14 +4,14 @@ extern crate gio;
 use gtk::prelude::*;
 
 const UPPER_INTERVAL: [&str; 15] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"];
-const LOWER_INTERVAL: [&str; 3] = ["2", "4", "8"];
+const LOWER_INTERVAL: [&str; 4] = ["1", "2", "4", "8"];
 
 fn selection_box(wrapper: &gtk::Box, values: &[&str]) {
   let select = gtk::ComboBoxText::new();
   for _i in 0..values.len() {
     select.append_text(values[_i]);
   }
-  select.set_active(Some(0));
+  select.set_active(Some(2));
   wrapper.add(&select);
 }
 
@@ -23,4 +23,5 @@ pub fn build(container: &gtk::Box) {
   wrapper.add(&divider);
   selection_box(&wrapper, &LOWER_INTERVAL);
   container.add(&wrapper);
+
 }
